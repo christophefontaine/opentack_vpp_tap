62431049a0bd0dacc425a24e7de1bb89599c784c
Christophe Fontaine <christophe.fontaine@qosmos.com>
2015-10-11 02:15:25 +0200

    Panel: Add a second button to disable the agent instead of a toggle button
    Signed-off-by: Christophe Fontaine <christophe.fontaine@qosmos.com>

--- openstack_panel/networkspotlight/tables.py
+++ openstack_panel/networkspotlight/tables.py
@@ -6,20 +6,33 @@ import logging
 LOG = logging.getLogger(__name__)
 
 
-class NetworkVisibilityToggleAction(tables.Action):
-    name = 'networkvisibility_toggle_action'
-    verbose_name = 'Toggle'
+class NetworkVisibilityEnableAction(tables.Action):
+    name = 'networkvisibility_enable_action'
+    verbose_name = 'Enable Monitoring'
 
     def single(self, table, request, object_id):
-        LOG.info(request)
         instance = table.get_object_by_id(object_id)
-        LOG.info(instance.visibility_enabled)
-        if instance.visibility_enabled == "True":
-            instance.visibility_enabled = "False"
-        else:
-            instance.visibility_enabled = "True"
-        LOG.info(instance.visibility_enabled)
-	novaclient(request).servers.set_meta_item(str(instance.id), 'nsa', instance.visibility_enabled)
+        instance.visibility_enabled = "True"
+	novaclient(request).servers.set_meta_item(str(instance.id), 'nsa', "True")
+
+
+class NetworkVisibilityDisableAction(tables.Action):
+    name = 'networkvisibility_disable_action'
+    verbose_name = 'Disable Monitoring'
+
+    def single(self, table, request, object_id):
+        instance = table.get_object_by_id(object_id)
+        instance.visibility_enabled = "False"
+	novaclient(request).servers.set_meta_item(str(instance.id), 'nsa', "False")
+
+
+class NetworkVisibilityLink(tables.LinkAction):
+    name = 'networkvisibility_go_to_kibana'
+    verbose_name = 'Go To Kibana'
+    
+    def get_link_url(self, datum=None):
+        LOG.info(self)
+        return 'http://kibana/'
 
 
 class InstancesTable(tables.DataTable):
@@ -28,12 +41,14 @@ class InstancesTable(tables.DataTable):
     image = tables.Column('image_name', verbose_name=_("Image Name"))
     zone = tables.Column('availability_zone',
                          verbose_name=_("Availability Zone"))
-    status = tables.Column("status", verbose_name=_("Status"))
+    status = tables.Column("status", verbose_name=_("Server Status"))
     visibility_enabled = tables.Column("visibility_enabled",
                                        verbose_name=_("Visibility Status"))
 
     class Meta:
         name = "instances"
         verbose_name = _("Instances")
-        row_actions = (NetworkVisibilityToggleAction,)
+        row_actions = (NetworkVisibilityEnableAction,
+                       NetworkVisibilityDisableAction,
+                       NetworkVisibilityLink,)
         status_columns = ['status', 'visibility_enabled']
