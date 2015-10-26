19743e39d5d8d78c566c81b1f57b2f4e95cd4dfd
Christophe Fontaine <christophe.fontaine@qosmos.com>
2015-10-26 17:06:10 +0100

    Replace Enable/Disable Buttons with a Toggle button
    Signed-off-by: Christophe Fontaine <christophe.fontaine@qosmos.com>

--- openstack_panel/networkspotlight/tables.py
+++ openstack_panel/networkspotlight/tables.py
@@ -12,8 +12,8 @@ class NetworkVisibilityEnableAction(tables.Action):
 
     def single(self, table, request, object_id):
         instance = table.get_object_by_id(object_id)
-        instance.visibility_enabled = "True"
-	novaclient(request).servers.set_meta_item(str(instance.id), 'nsa', "True")
+        instance.visibility_enabled = 'True'
+	novaclient(request).servers.set_meta_item(str(instance.id), 'nsa', 'True')
 
 
 class NetworkVisibilityDisableAction(tables.Action):
@@ -22,8 +22,50 @@ class NetworkVisibilityDisableAction(tables.Action):
 
     def single(self, table, request, object_id):
         instance = table.get_object_by_id(object_id)
-        instance.visibility_enabled = "False"
-	novaclient(request).servers.set_meta_item(str(instance.id), 'nsa', "False")
+        instance.visibility_enabled = 'False'
+	novaclient(request).servers.set_meta_item(str(instance.id), 'nsa', 'False')
+
+
+class ToggleNetworkVisibility(tables.BatchAction):
+    name = "toggle_networkvisibility"
+    icon = "pause"
+
+    ENABLE = 0
+    DISABLE = 1
+
+    @staticmethod
+    def action_present(count):
+        LOG.warning("action_present")
+        return (u"Enable Agent", u"Disable Agent",)
+
+    @staticmethod
+    def action_past(count):
+        LOG.warning("action_past")
+        return (u"Agent enabled", u"Agent disabled",)
+
+    def allowed(self, request, instance=None):
+        LOG.warning("allowed")
+        if not instance:
+            return False
+
+        self.visibility_enabled = (instance.to_dict()['metadata']['nsa'] == 'True') or False
+        instance.visibility_enabled = self.visibility_enabled
+        if self.visibility_enabled:
+            self.current_present_action = ToggleNetworkVisibility.DISABLE
+        else:
+            self.current_present_action = ToggleNetworkVisibility.ENABLE
+        return True
+
+    def action(self, request, instance_id):
+        LOG.warning("action")
+        if self.visibility_enabled:
+            self.visibility_enabled = False
+            novaclient(request).servers.set_meta_item(instance_id, 'nsa', 'False')
+            self.current_past_action = ToggleNetworkVisibility.DISABLE
+        else:
+            self.visibility_enabled = True
+            novaclient(request).servers.set_meta_item(instance_id, 'nsa', 'True')
+            self.current_past_action = ToggleNetworkVisibility.ENABLE
 
 
 class NetworkVisibilityLink(tables.LinkAction):
@@ -42,13 +84,12 @@ class InstancesTable(tables.DataTable):
     zone = tables.Column('availability_zone',
                          verbose_name=_("Availability Zone"))
     status = tables.Column("status", verbose_name=_("Server Status"))
+    addresses = tables.Column("addresses", verbose_name=_("Addresses"))
     visibility_enabled = tables.Column("visibility_enabled",
                                        verbose_name=_("Visibility Status"))
 
     class Meta:
         name = "instances"
         verbose_name = _("Instances")
-        row_actions = (NetworkVisibilityEnableAction,
-                       NetworkVisibilityDisableAction,
-                       NetworkVisibilityLink,)
+        row_actions = (ToggleNetworkVisibility, NetworkVisibilityLink,)
         status_columns = ['status', 'visibility_enabled']
