93703764a57d733e304e84ed87fe19a7c50fe593
Christophe Fontaine <christophe.fontaine@qosmos.com>
2015-10-30 19:58:36 +0100

    Register to other metadatas
    Signed-off-by: Christophe Fontaine <christophe.fontaine@qosmos.com>

--- network_spotlight_agent/conf_metadata.py
+++ network_spotlight_agent/conf_metadata.py
@@ -1,6 +1,8 @@
 from protocols import protocols as p
 
 SUBSCRIBED_METADATAS = [
-    p.http.method,
-    p.http.host,
-    p.http.uri]
+    p.http.method, p.http.host, p.http.uri,
+    p.ftp.login, p.ftp.password, p.ftp.filename,
+    p.bittorrent.torrent_filename, 
+    p.dns.query_name, p.dns.host_addr,
+]
