15a5801df7c88043bcdacf6e5e929bc2474084ae
Christophe Fontaine <christophe.fontaine@qosmos.com>
2015-11-18 18:42:03 +0100

    Update install_elk to start mango-connector and kibana automatically
    Signed-off-by: Christophe Fontaine <christophe.fontaine@qosmos.com>

--- install_elk.sh
+++ install_elk.sh
@@ -30,6 +30,54 @@ rpm -i logstash-2.0.0-1.noarch.rpm elasticsearch-2.0.0.rpm
 systemctl enable elasticsearch.service
 systemctl start elasticsearch.service
 
-echo "Please run manually :"
+echo "Installing mongo-connectord"
 echo "$ mongo-connector --auto-commit-interval=0 -m 127.0.0.1:27017 -t 127.0.0.1:9200 -d elastic_doc_manager"
+
+cat <<EOF > /etc/systemd/system/mongo-connectord.service
+[Unit]
+Description=Mongo Connector Daemon
+After=syslog.target network.target rabbitmq-server.service
+
+[Service]
+StandardOutput=syslog
+StandardError=syslog
+ExecStart=/usr/bin/bash -c "/usr/bin/mongo-connector --auto-commit-interval=0 -m `gethostip -d $HOSTNAME`:27017 -t 127.0.0.1:9200 -d elastic_doc_manager --stdout"
+ExecReload=/bin/kill -HUP \$MAINPID
+KillMode=process
+Restart=always
+RestartSec=10s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable /etc/systemd/system/mongo-connectord.service
+systemctl start mongo-connectord.service
+
+echo "Installing kibana-launcher"
 echo "$ ./kibana-4.2.0-linux-x64/bin/kibana"
+
+
+cat <<EOF > /etc/systemd/system/kibana.service
+[Unit]
+Description=Kibana daemon
+After=syslog.target network.target
+ConditionPathExists=`pwd`
+
+[Service]
+StandardOutput=syslog
+StandardError=syslog
+ExecStart=`pwd`/kibana-4.2.0-linux-x64/bin/kibana
+ExecReload=/bin/kill -HUP \$MAINPID
+KillMode=process
+Restart=always
+RestartSec=10s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable /etc/systemd/system/kibana.service
+systemctl start kibana.service
+
+
