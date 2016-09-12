1f95ab3f8daeb8aa71fd25bcacdff8a8401b5db8
Christophe Fontaine <christophe.fontaine@qosmos.com>
2016-09-12 18:19:34 +0200

    Rename to vprobe
    Signed-off-by: Christophe Fontaine <christophe.fontaine@qosmos.com>

---
+++ devstack/plugin.sh
@@ -0,0 +1,52 @@
+VPROBE_DASH_DIR=$(cd $(dirname $BASH_SOURCE)/.. && pwd)
+
+function install_vprobe_dashboard {
+    setup_develop ${VPROBE_DASH_DIR}
+}
+
+function configure_vprobe_dashboard {
+    cp -a ${VPROBE_DASH_DIR}/vprobe_dashboard/enabled/* ${DEST}/horizon/openstack_dashboard/local/enabled/
+    # NOTE: If locale directory does not exist, compilemessages will fail,
+    # so check for an existence of locale directory is required.
+    if [ -d ${VPROBE_DASH_DIR}/vprobe_dashboard/locale ]; then
+        (cd ${VPROBE_DASH_DIR}/vprobe_dashboard; DJANGO_SETTINGS_MODULE=openstack_dashboard.settings ../manage.py compilemessages)
+    fi
+}
+
+# check for service enabled
+if is_service_enabled vprobe-dashboard; then
+
+    if [[ "$1" == "stack" && "$2" == "pre-install"  ]]; then
+        # Set up system services
+        # no-op
+        :
+
+    elif [[ "$1" == "stack" && "$2" == "install"  ]]; then
+        # Perform installation of service source
+        echo_summary "Installing vprobe Dashboard"
+        install_vprobe_dashboard
+
+    elif [[ "$1" == "stack" && "$2" == "post-config"  ]]; then
+        # Configure after the other layer 1 and 2 services have been configured
+        echo_summary "Configuring vprobe Dashboard"
+        configure_vprobe_dashboard
+
+    elif [[ "$1" == "stack" && "$2" == "extra"  ]]; then
+        # Initialize and start the app-catalog-ui service
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "unstack"  ]]; then
+        # Shut down app-catalog-ui services
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "clean"  ]]; then
+        # Remove state and transient data
+        # Remember clean.sh first calls unstack.sh
+        # no-op
+        :
+    fi
+fi
--- setup.py
+++ setup.py
@@ -1,36 +1,13 @@
-#!/usr/bin/env python
-import os
-from setuptools import setup, find_packages
+import setuptools
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
-setup(
-    name = "networkspotlight",
-    version = "0.1.a0.dev-4",
-    author = "Christophe Fontaine",
-    author_email = "christophe.fontaine@qosmos.com",
-    description = (""),
-    license = "Proprietary",
-    keywords = "",
-    url = "",
-    long_description=read('README'),
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Environment :: Plugins",
-        "License :: Other/Proprietary License",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Topic :: System :: Networking :: Monitoring",
-        "Topic :: Utilities"
-    ],
-    provides = ['networkspotlight'],
-    zip_safe=False,
-    packages=['networkspotlight'],
-    package_data={'networkspotlight': ['templates/networkspotlight/*', 'static/networkspotlight/*']},
-    data_files=[('/usr/share/openstack-dashboard/openstack_dashboard/local/enabled', ['_99_nsa.py'])],
-)
+setuptools.setup(
+    setup_requires=['pbr>=1.8'],
+    pbr=True)
