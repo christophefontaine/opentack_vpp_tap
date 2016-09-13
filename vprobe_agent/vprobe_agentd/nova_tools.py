fd168676dad5b1eaec886fc0505bb172b7454b39
Christophe Fontaine <christophe.fontaine@qosmos.com>
2016-09-13 12:37:02 +0200

    Update authentication to keystone v3
    Signed-off-by: Christophe Fontaine <christophe.fontaine@qosmos.com>

--- vprobe_agent/vprobe_agentd/nova_tools.py
+++ vprobe_agent/vprobe_agentd/nova_tools.py
@@ -1,23 +1,34 @@
 #!/usr/bin/env python
+from keystoneauth1.identity import v3
+from keystoneauth1 import session
 from novaclient import client
 from novaclient.v2.servers import ServerManager
 import socket
 import ConfigParser
 
+
 def _get_client():
     config = ConfigParser.ConfigParser()
     config.read('/etc/nova/nova.conf')
-    AUTH_URL = config.get('keystone_authtoken', 'auth_uri')
-    USERNAME = config.get('keystone_authtoken', 'admin_user')
-    PASSWORD = config.get('keystone_authtoken', 'admin_password')
-    PROJECT_ID = config.get('keystone_authtoken', 'admin_tenant_name')
-    VERSION = '2'
-    return client.Client(VERSION, USERNAME, PASSWORD, PROJECT_ID, AUTH_URL)
+    AUTH_URL = config.get('keystone_authtoken', 'auth_uri') + '/v3'
+    USERNAME = config.get('keystone_authtoken', 'username')
+    PASSWORD = config.get('keystone_authtoken', 'password')
+    PROJECT_NAME = config.get('keystone_authtoken', 'project_name')
+    PROJECT_DOMAIN_NAME = config.get('keystone_authtoken', 'project_domain_name')
+    USER_DOMAIN_NAME = config.get('keystone_authtoken', 'user_domain_name')
 
+    VERSION = '2.1'
+    auth = v3.Password(auth_url=AUTH_URL,
+                      username=USERNAME,
+                      password=PASSWORD,
+                      project_name=PROJECT_NAME,
+                      user_domain_name=USER_DOMAIN_NAME,
+                      project_domain_name=PROJECT_DOMAIN_NAME)
+    sess = session.Session(auth=auth)
+    return client.Client(VERSION, session=sess)
 
 def _get_vms(nova):
     return nova.servers.list(search_opts={'all_tenants': 1, 'host': socket.gethostname()})
-
 
 def enable_vprobes():
     nova = _get_client()
